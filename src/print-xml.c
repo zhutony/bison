b7c724644f575dd98917188b0f487c3b23557c99
Akim Demaille <akim.demaille@gmail.com>
2020-02-22 21:55:23 +0100

    multistart: adjust computation of initial core and adjust reports
    Currently the core of the initial state is limited to the single rule
    on $accept.
    
    * src/lr0.c (generate_states): There may now be several rules on
    $accept.
    
    * src/graphviz.c (conclude_red): Recognize "final" transitions by the
    fact that we reduce to "$accept".
    * src/print.c (print_reduction): Likewise.
    * src/print-xml.c (print_reduction): Likewise.

--- src/print-xml.c
+++ src/print-xml.c
@@ -212,16 +212,17 @@ static void
 print_reduction (FILE *out, int level, char const *lookahead_token,
                  rule *r, bool enabled)
 {
-  if (r->number)
+  const bool final = r->lhs->symbol == accept;
+  if (final)
+    xml_printf (out, level,
+                "<reduction symbol=\"%s\" rule=\"accept\" enabled=\"%s\"/>",
+                xml_escape (lookahead_token),
+                enabled ? "true" : "false");
+  else
     xml_printf (out, level,
                 "<reduction symbol=\"%s\" rule=\"%d\" enabled=\"%s\"/>",
                 xml_escape (lookahead_token),
                 r->number,
-                enabled ? "true" : "false");
-  else
-    xml_printf (out, level,
-                "<reduction symbol=\"%s\" rule=\"accept\" enabled=\"%s\"/>",
-                xml_escape (lookahead_token),
                 enabled ? "true" : "false");
 }
 
